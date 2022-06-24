9d42f274366f81db5b7d6d3a8073d99cfe0d4ab3
Xeon <88829846+DGXeon@users.noreply.github.com>
2022-06-24 11:43:50 +0530

    Update config.js

--- config.js
+++ config.js
@@ -25,7 +25,7 @@ const fs = require("fs")
 const chalk = require("chalk")
 
 //aumto functioner
-global.autoTyping = true //auto tying in gc (true to on, false to off)
+global.autoTyping = false //auto tying in gc (true to on, false to off)
 global.autoreadpmngc = false //auto reading in gc and pm (true to on, false to off)
 global.autoReadGc = true //auto reading in gc (true to on, false to off)
 global.autoReadAll = false // auto reading in all pchat gc and status (true to on, false to off)
