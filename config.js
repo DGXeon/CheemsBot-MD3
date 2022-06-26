e5a7130dff9a7a20ff964861bf355108a552f69f
Akhil <102639736+DreamyPrince@users.noreply.github.com>
2022-06-26 18:39:33 +0530

    Update config.js

--- config.js
+++ config.js
@@ -25,11 +25,11 @@ const fs = require("fs")
 const chalk = require("chalk")
 
 //aumto functioner
-global.autoTyping = false //auto tying in gc (true to on, false to off)
+global.autoTyping = true //auto tying in gc (true to on, false to off)
 global.autoreadpmngc = false //auto reading in gc and pm (true to on, false to off)
 global.autoReadGc = true //auto reading in gc (true to on, false to off)
-global.autoReadAll = false // auto reading in all pchat gc and status (true to on, false to off)
-global.autoRecord = false //auto recording (true to on, false to off)
+global.autoReadAll = true // auto reading in all pchat gc and status (true to on, false to off)
+global.autoRecord = true //auto recording (true to on, false to off)
 global.available = true //auto available (true to on, false to off)
 
 //documents variants
@@ -41,26 +41,26 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['ur number'] //ur owner number
-global.ownername = "ur name"
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.owner = ['+919778062036'] //ur owner number
+global.ownername = "Akhil"
+global.ytname = "AlonePhilic" //ur yt chanel name
+global.socialm = "itzme_lucifer01" //ur github or insta name
+global.location = "India, Kerala, Trivandrum" //ur location
 
 //bot bombdy
-global.botname = "ur bot name" //ur bot name
-global.websitex = "https://youtu.be/xn9RatOrbuI" //ur website
-global.vidmenu = { url: 'https://a.uguu.se/zlzQZvXH.mp4' } //gif and video menu
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n🦄🦄🦄 "
-global.author = "🦄Dream Guy Xeon"
-global.themeemoji = "🐶"
-global.reactmoji = "😘"
-global.ownertag = ['ur number'] //ur owner tag
-global.ownernummenu = ['ur number'] //ur owner number in menu and all
-global.watermark = "Xeon Bot Inc." //ur watermark
-global.botscript = "https://github.com/DGXeon/CheemsBot-MD3"
-global.linkz1 = "https://chat.whatsapp.com/HYj9wu5Jrv6CROxyeQbHoS"
-global.linkz2 = "https://chat.whatsapp.com/EcycNbJFCVT5ZsG9xIGkqd"
+global.botname = "Evania" //ur bot name
+global.websitex = "https://github.com/DreamyPrince" //ur website
+global.vidmenu = { url: 'https://telegra.ph/file/d91135bfb16d081a84bb6.jpg' } //gif and video menu
+global.packname = "DreamyPrince \n\n\n\n\nStickers"
+global.author = "Akhil"
+global.themeemoji = "😊"
+global.reactmoji = "☺️"
+global.ownertag = ['919778062036'] //ur owner tag
+global.ownernummenu = ['919778026036'] //ur owner number in menu and all
+global.watermark = "DreamyPrince" //ur watermark
+global.botscript = "https://github.com/DreamyPrince/CheemsBot-MD3"
+global.linkz1 = "https://wa.me/+919778062036"
+global.linkz2 = "https://wa.me/+919778062036"
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
@@ -92,7 +92,7 @@ global.ntilink = []
 global.lolhuman = "KaysaS"
 global.sessionName = "session"
 global.antitags = true
-global.prefa = ['','!','.','🐦','🐤','🗿']
+global.prefa = ['','!','.','🌚','😊','⭐']
 global.mess = {
     success: 'Done✓',
     admin: 'This feature is only for admins!',
@@ -110,8 +110,8 @@ global.mess = {
 }
 global.limitawal = {
     premium: "Infinity",
-    free: 2,
-    monayawal: 1000
+    free: 200,
+    monayawal: 100000
 }
    global.rpg = {
    darahawal: 100,
@@ -132,7 +132,7 @@ global.APIs = {
 
 // key
 global.APIKeys = {
-	'https://zenzapis.xyz': 'Your Key',
+	'https://zenzapis.xyz': '1367a5d936',
 }
 //menu logo maker
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
